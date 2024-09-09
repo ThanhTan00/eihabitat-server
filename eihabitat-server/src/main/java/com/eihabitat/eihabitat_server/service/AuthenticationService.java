b540cda068dda2701cf735715951183e12951ba1
thanhdatt21 <dat.nguyen.cit19@eiu.edu.vn>
2024-09-09 20:14:35 +0700

    18 refresh

--- eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/service/AuthenticationService.java
+++ eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/service/AuthenticationService.java
@@ -42,9 +42,19 @@ import java.util.UUID;
 public class AuthenticationService {
     UserRepository userRepository;
     InvalidatedTokenRepository invalidatedTokenRepository;
+
     @NonFinal
     @Value("${jwt.signerKey}")
     protected String SIGNER_KEY;
+
+    @NonFinal
+    @Value("${jwt.valid-duration}")
+    protected long VALID_DURATION;
+
+    @NonFinal
+    @Value("${jwt.refreshable-duration}")
+    protected long REFRESHABLE_DURATION;
+
 
     public IntrospectResponse introspect(IntrospectReq requset) throws JOSEException, ParseException {
         var token = requset.getToken();
@@ -52,7 +62,7 @@ public class AuthenticationService {
         boolean isValid = true;
 
         try {
-            verifyToken(token);
+            verifyToken(token, false);
         } catch (AppException e) {
            isValid = false;
         }
@@ -81,7 +91,8 @@ public class AuthenticationService {
                 .build();
     }
     public void logout(LogoutRequest request) throws JOSEException, ParseException  {
-        var signToken = verifyToken(request.getToken());
+        try {
+        var signToken = verifyToken(request.getToken(), true);
 
         String jit = signToken.getJWTClaimsSet().getJWTID();
         Date expiryTime = signToken.getJWTClaimsSet().getExpirationTime();
@@ -92,15 +103,22 @@ public class AuthenticationService {
             .build();
         
             invalidatedTokenRepository.save(invalidatedToken);
+
+        } catch (AppException exception) {
+            log.info("Token already expired");
+        }
     }
 
-    private SignedJWT verifyToken(String token) throws JOSEException, ParseException {
+    private SignedJWT verifyToken(String token, boolean isRefresh) throws JOSEException, ParseException {
 
         JWSVerifier verifier = new MACVerifier(SIGNER_KEY.getBytes());
 
         SignedJWT signedJWT = SignedJWT.parse(token);
 
-        Date expireTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+        Date expireTime = (isRefresh) 
+            ? new Date(signedJWT.getJWTClaimsSet().getIssueTime().toInstant()
+            .plus(REFRESHABLE_DURATION, ChronoUnit.SECONDS).toEpochMilli()) 
+            : signedJWT.getJWTClaimsSet().getExpirationTime();
 
         var verified = signedJWT.verify(verifier);
 
@@ -117,7 +135,7 @@ public class AuthenticationService {
     public AuthenticationResponse refreshToken(RefreshRequest request) 
             throws JOSEException, ParseException {
 
-        var signedJWT = verifyToken(request.getToken());
+        var signedJWT = verifyToken(request.getToken(), true);
 
         var jit = signedJWT.getJWTClaimsSet().getJWTID();
         var expiryTime = signedJWT.getJWTClaimsSet().getExpirationTime();
@@ -129,9 +147,9 @@ public class AuthenticationService {
 
         invalidatedTokenRepository.save(invalidatedToken);
 
-        var username = signedJWT.getJWTClaimsSet().getSubject();
+        var email = signedJWT.getJWTClaimsSet().getSubject();
 
-        var user = userRepository.findByUsername(username).orElseThrow(
+        var user = userRepository.findByEmail(email).orElseThrow(
             () -> new AppException(ErrorCode.UNAUTHENTICATED));
 
         var token = generateToken(user);
@@ -151,7 +169,7 @@ public class AuthenticationService {
                 .issuer("eihabitat")
                 .issueTime(new Date())
                 .expirationTime(new Date(
-                        Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
+                        Instant.now().plus(VALID_DURATION, ChronoUnit.SECONDS).toEpochMilli()
                 ))
                 .jwtID(UUID.randomUUID().toString())
                 .claim("scope",buildScope(user))
