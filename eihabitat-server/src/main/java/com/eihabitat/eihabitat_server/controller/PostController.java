233009becfde255a8554b33dd16785f9c2fdddc8
ThanhTan00 <131366907+ThanhTan00@users.noreply.github.com>
2024-09-16 21:19:16 +0700

    update resolve conflict

--- eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/controller/PostController.java
+++ eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/controller/PostController.java
@@ -6,13 +6,15 @@ import com.eihabitat.eihabitat_server.dto.request.PostCreationReq;
 import com.eihabitat.eihabitat_server.dto.request.PostUpdateReq;
 import com.eihabitat.eihabitat_server.dto.response.PostContentResponse;
 import com.eihabitat.eihabitat_server.dto.response.PostResponse;
-import com.eihabitat.eihabitat_server.service.PostContentService;
+//import com.eihabitat.eihabitat_server.service.PostContentService;
 import com.eihabitat.eihabitat_server.service.PostService;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
