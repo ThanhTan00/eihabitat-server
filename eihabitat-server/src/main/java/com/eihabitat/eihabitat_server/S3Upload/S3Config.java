ee2ded28a615ba8d1676b62835a4f2db73b2bc43
ThanhTan00 <131366907+ThanhTan00@users.noreply.github.com>
2024-10-11 00:46:55 +0700

    update

--- eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/S3Upload/S3Config.java
+++ eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/S3Upload/S3Config.java
@@ -1,32 +1,33 @@
-package com.eihabitat.eihabitat_server.S3Upload;
-
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class S3Config {
-
-    @Value("${aws.s3.access-key}")
-    private String accessKey;
-
-    @Value("${aws.s3.secret-key}")
-    private String secretKey;
-
-    @Value("${aws.s3.region}")
-    private String region;
-
-    @Bean
-    public S3Client s3Client() {
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
-                .build();
-    }
-}
-
-
+//package com.eihabitat.eihabitat_server.S3Upload;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+//import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+//import software.amazon.awssdk.regions.Region;
+//import software.amazon.awssdk.services.s3.S3Client;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class S3Config {
+//
+//    @Value("${aws.s3.access-key}")
+//    private String accessKey;
+//
+//    @Value("${aws.s3.secret-key}")
+//    private String secretKey;
+//
+//    @Value("${aws.s3.region}")
+//    private String region;
+//
+//    @Bean
+//    public S3Client s3Client() {
+//        return S3Client.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+//                .build();
+//    }
+//}
+//
+//
--- eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/S3Upload/S3Service.java
+++ eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/S3Upload/S3Service.java
@@ -1,53 +1,55 @@
-package com.eihabitat.eihabitat_server.S3Upload;
-
-import software.amazon.awssdk.core.sync.ResponseTransformer;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-@Service
-public class S3Service {
-
-    @Autowired
-    private S3Client s3Client;
-
-    @Value("${aws.s3.bucket-name}")
-    private String bucketName;
-
-    public String uploadFile(MultipartFile file) throws IOException {
-        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
-
-        // Upload file to S3 bucket
-        s3Client.putObject(
-                PutObjectRequest.builder()
-                        .bucket(bucketName)
-                        .key(fileName)
-                        .build(),
-                software.amazon.awssdk.core.sync.RequestBody.fromBytes(file.getBytes())
-        );
-
-        return fileName;
-    }
-
-     public byte[] downloadFile(String fileName) throws IOException {
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(bucketName)
-                .key(fileName)
-                .build();
-
-        // Download the file from S3 and return it as a byte array
-        try (InputStream inputStream = s3Client.getObject(getObjectRequest, ResponseTransformer.toInputStream())) {
-            return inputStream.readAllBytes();
-        }
-    }
-
-}
-
-
+//package com.eihabitat.eihabitat_server.S3Upload;
+//
+//import lombok.RequiredArgsConstructor;
+//import software.amazon.awssdk.core.sync.ResponseTransformer;
+//import software.amazon.awssdk.services.s3.S3Client;
+//import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+//import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class S3Service {
+//
+//    @Autowired
+//    private S3Client s3Client;
+//
+//    @Value("${aws.s3.bucket-name}")
+//    private String bucketName;
+//
+//    public String uploadFile(MultipartFile file) throws IOException {
+//        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+//
+//        // Upload file to S3 bucket
+//        s3Client.putObject(
+//                PutObjectRequest.builder()
+//                        .bucket(bucketName)
+//                        .key(fileName)
+//                        .build(),
+//                software.amazon.awssdk.core.sync.RequestBody.fromBytes(file.getBytes())
+//        );
+//
+//        return fileName;
+//    }
+//
+//     public byte[] downloadFile(String fileName) throws IOException {
+//        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+//                .bucket(bucketName)
+//                .key(fileName)
+//                .build();
+//
+//        // Download the file from S3 and return it as a byte array
+//        try (InputStream inputStream = s3Client.getObject(getObjectRequest, ResponseTransformer.toInputStream())) {
+//            return inputStream.readAllBytes();
+//        }
+//    }
+//
+//}
+//
+//
