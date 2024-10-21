f5d369f09e4393a15be86a6479c100769b50c7bf
ThanhTan00 <131366907+ThanhTan00@users.noreply.github.com>
2024-10-21 22:00:22 +0700

    update

--- eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/service/UserService.java
+++ eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/service/UserService.java
@@ -1,70 +1,136 @@
 package com.eihabitat.eihabitat_server.service;
 
-import com.eihabitat.eihabitat_server.dto.request.AuthenticationReq;
 import com.eihabitat.eihabitat_server.dto.request.UserCreationReq;
 import com.eihabitat.eihabitat_server.dto.request.UserUpdateReq;
-import com.eihabitat.eihabitat_server.dto.response.UserDemoResponse;
 import com.eihabitat.eihabitat_server.dto.response.UserFollowerResponse;
 import com.eihabitat.eihabitat_server.dto.response.UserResponse;
+import com.eihabitat.eihabitat_server.entity.EmailConfirmationToken;
 import com.eihabitat.eihabitat_server.entity.Role;
 import com.eihabitat.eihabitat_server.entity.User;
 import com.eihabitat.eihabitat_server.exception.AppException;
 import com.eihabitat.eihabitat_server.exception.ErrorCode;
 import com.eihabitat.eihabitat_server.mapper.UserMapper;
+import com.eihabitat.eihabitat_server.repository.EmailConfirmationRepository;
 import com.eihabitat.eihabitat_server.repository.RoleRepository;
 import com.eihabitat.eihabitat_server.repository.UserRepository;
+
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+import jakarta.mail.MessagingException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.Hibernate;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.keygen.BytesKeyGenerator;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import software.amazon.awssdk.services.s3.endpoints.internal.Value;
 
+import java.nio.charset.Charset;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 public class UserService {
+    EmailConfirmationRepository emailConfirmationRepository;
     UserRepository userRepository;
     UserMapper userMapper;
     PasswordEncoder passwordEncoder;
     RoleRepository roleRepository;
     UserFollowService userFollowService;
+    EmailService emailService;
+    BytesKeyGenerator DEFAULT_TOKEN_GENERATOR = KeyGenerators.secureRandom(15);
+    Charset US_ASCII = StandardCharsets.US_ASCII;
 
-    public UserResponse createUser(UserCreationReq request) {
-        if (userRepository.existsByEmail(request.getEmail()))
-            throw new AppException(ErrorCode.EMAIL_EXISTED);
-        if (userRepository.existsByProfileName(request.getProfileName()))
+    public String confirmEmail(UserCreationReq request) throws MessagingException {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new AppException(ErrorCode.USER_EXISTED);
+        }
+        if (userRepository.existsByProfileName(request.getProfileName())) {
             throw new AppException(ErrorCode.USERNAME_EXISTED);
+        }
+        String tokenValue = new String(Base64.getUrlEncoder().encode(DEFAULT_TOKEN_GENERATOR.generateKey()), US_ASCII);
+        EmailConfirmationToken emailConfirmationToken = EmailConfirmationToken.builder()
+                .email(request.getEmail())
+                .profileName(request.getProfileName())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .password(request.getPassword())
+                .expiryTime(LocalDateTime.now().plusHours(24))
+                .token(tokenValue)
+                .signupDate(LocalDateTime.now())
+                .verified(false)
+                .build();
+        emailConfirmationRepository.save(emailConfirmationToken);
+        emailService.sendConfirmationEmail(emailConfirmationToken);
+        return "Please check your email to confirm your email address";
+    }
+
+
+    public boolean createUser(UserCreationReq request) throws MessagingException {
+        // Check if email or profile name already exists
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new AppException(ErrorCode.USER_EXISTED);
+        }
+        if (userRepository.existsByProfileName(request.getProfileName())) {
+            throw new AppException(ErrorCode.USERNAME_EXISTED);
+        }
+
+        // Map the request to a User entity and set additional properties
         User user = userMapper.toUser(request);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setProfileAvatar("asset/images/default-avatar.png");
-
-        LocalDate date = LocalDate.now();
-        user.setSignupDate(date);
-
-        Role userRole = roleRepository.findById("USER").orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        // Retrieve the role and set it to the user
+        Role userRole = roleRepository.findById("USER")
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
         HashSet<Role> userRoles = new HashSet<>();
-
         userRoles.add(userRole);
         user.setRoles(userRoles);
+        user.setSignupDate(LocalDate.now());
 
-        return userMapper.toUserResponse(userRepository.save(user));
+        // Save the user to the database before sending the email
+        user = userRepository.save(user);
+
+        // Return the UserResponse object
+        return true;
     }
+
+    public boolean verifyUser(String token) throws MessagingException {
+        log.info(token);
+        EmailConfirmationToken emailToken = emailConfirmationRepository.findEmailConfirmationTokenByToken(token).orElseThrow(() -> new AppException(ErrorCode.EMAIL_CONFIRM_TOKEN_INVALID));
+        log.info(emailToken.toString());
+        // Check if the token has already been used or verified
+        if (emailToken.isVerified()) {
+            return false;
+        }
+
+        // Check if the token has expired
+        if (emailToken.getExpiryTime().isBefore(LocalDateTime.now())) {
+            return false;
+        }
+
+        // Mark the token as verified and save the changes
+        emailToken.setVerified(true);
+        emailConfirmationRepository.save(emailToken);
+
+        UserCreationReq request = UserCreationReq.builder()
+                .email(emailToken.getEmail())
+                .profileName(emailToken.getProfileName())
+                .firstName(emailToken.getFirstName())
+                .lastName(emailToken.getLastName())
+                .password(emailToken.getPassword())
+                .build();
+
+        return createUser(request);
+    }
+
 
     @PreAuthorize("hasRole('ADMIN')")
     public List<UserResponse> getUsers() {
@@ -73,11 +139,13 @@ public class UserService {
         userRepository.findAll().forEach(user -> {
             userResponses.add(userMapper.toUserResponse(user));
         });
-       return userResponses;
+        return userResponses;
     }
 
-    public UserDemoResponse getUserDemo(String email) {
-        return userMapper.toUserDemoResponse(userRepository.findByEmail(email).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND)));
+    @PostAuthorize("returnObject.email == authentication.name")
+    public UserResponse getUser(String id) {
+        log.info("Getting user with id {}", id);
+        return userMapper.toUserResponse(userRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND)));
     }
 
     public UserResponse updateUser(UserUpdateReq req) {
@@ -109,12 +177,4 @@ public class UserService {
         userResponse.setFollowing(listFollowing.size());
         return userResponse;
     }
-    public User getUserWithRoles(String email) {
-        User user = userRepository.findByEmail(email).orElse(null);
-        if (user != null){
-            Hibernate.initialize(user.getRoles());
-        }
-        return user;
-    }
-
 }
