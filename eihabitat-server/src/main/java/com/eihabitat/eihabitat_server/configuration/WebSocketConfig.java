398c89ee220c102202b2743bd030fdb009e381cb
ThanhTan00 <131366907+ThanhTan00@users.noreply.github.com>
2024-11-19 01:16:01 +0700

    update

--- eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/configuration/WebSocketConfig.java
+++ eihabitat-server/src/main/java/com/eihabitat/eihabitat_server/configuration/WebSocketConfig.java
@@ -1,22 +1,21 @@
 package com.eihabitat.eihabitat_server.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic", "/queue");
-        config.setApplicationDestinationPrefixes("/app");
-        config.setUserDestinationPrefix("/user");
+        config.enableSimpleBroker("/topic"); // Enable broadcasting on /topic
+        config.setApplicationDestinationPrefixes("/app"); // Prefix for message mappings
     }
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
